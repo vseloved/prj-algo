aa6123c0ed74c1fc60cded4603440c61138c8774
Alex Pushkin <mallaka13@gmail.com>
2016-05-02 14:32:58 +0300

    converted to hex

---
+++ tasks/mort/lectures/selectionSort.py
@@ -0,0 +1,20 @@
+def insertion_sort(a):
+    print(a)
+    lenA = len(a)
+    for key in range(0, lenA - 1):
+        if a[key] > a[key + 1]:
+            temp = a[key + 1]
+            a[key + 1] = a[key]
+            a[key] = temp
+            for rkey in range(key, 1, -1):
+                if a[rkey] < a[rkey - 1]:
+                    temp = a[rkey - 1]
+                    a[rkey - 1] = a[rkey]
+                    a[rkey] = temp
+                else:
+                    break
+        print(a)
+
+a = [2, 8, 6, 4, 7, 4, 4, 123, 14, 5, 52, 23]
+insertion_sort(a)
+print(a)
