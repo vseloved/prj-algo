0d74bab306b4c827b20d851bb439d383cabc382b
Andriy Sokolovskiy <me@asokolovskiy.com>
2016-05-06 18:10:38 +0300

    Added heap visualization util

--- tasks/coldmind/task06/heapsort.py
+++ tasks/coldmind/task06/heapsort.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 
@@ -72,6 +73,27 @@ def is_heap(arr, i, el_count):
     return False
 
 
+def visualize_heap(heap, width=48, fill_with="."):
+    """
+    Example output:
+    .......................10.......................
+    ...........8.......................9............
+    .....7...........3...........5...........8......
+    """
+    result_str = ""
+    last_row = -1
+    for i, n in enumerate(heap):
+        row = int(math.log(i + 1, 2))
+        if row != last_row:
+            result_str += "\n"
+        last_row = row
+        columns = 2**row
+        col_width = int(math.floor((width * 1.0) / columns))
+        result_str += str(n).center(col_width, fill_with)
+    print(result_str)
+    return result_str
+
+
 def heapsort(arr):
     heap = make_heap(arr, [])
     while(len(heap)):
@@ -82,18 +104,26 @@ def heapsort(arr):
 heap1 = [10, 8, 8, 7, 3, 5]
 heap_push(heap1, 9)
 assert heap1 == [10, 8, 9, 7, 3, 5, 8]
+visualize_heap(heap1)
+print("\n\n")
 
 heap2 = [10, 8, 9, 7, 3, 5, 8, 7]
 heap_push(heap2, 9)
 assert heap2 == [10, 9, 9, 8, 3, 5, 8, 7, 7]
+visualize_heap(heap2)
+print("\n\n")
 
 heap3 = []
 heap3 = make_heap([1, 3, 5, 7, 7, 8, 8, 9, 9, 10], heap3)
 assert heap3 == [10, 9, 8, 8, 9, 3, 7, 1, 7, 5]
+visualize_heap(heap3)
+print("\n\n")
 
 heap4 = [12, 8, 11, 7, 3, 5]
 assert heap_pop(heap4) == 12
 assert heap4 == [11, 8, 5, 7, 3]
+visualize_heap(heap4)
+print("\n\n")
 
 arr1 = [0, -5, -23, 19, 6, 3, 6, 6, 7, 1, 0, 9, 1, 5, 7, 3, 3, 2]
 assert list(heapsort(arr1[:])) == sorted(arr1[:], reverse=True)
