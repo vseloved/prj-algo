35e0b0f73d5dd97f63131958f635b7b0d2e8effc
Alex Pushkin <mallaka13@gmail.com>
2016-05-13 07:37:51 +0300

    q

--- tasks/mort/task5/selectionSort.py
+++ tasks/mort/task5/selectionSort.py
@@ -1,19 +1,12 @@
 def selection_sort(a):
-    print(a)
-    lenA = len(a)
-    for key in range(0, lenA - 1):
+    for key in range(0, len(a) - 1):
         if a[key] > a[key + 1]:
-            temp = a[key + 1]
-            a[key + 1] = a[key]
-            a[key] = temp
+            a[key], a[key + 1] = a[key + 1], a[key]
             for rkey in range(key, 1, -1):
                 if a[rkey] < a[rkey - 1]:
-                    temp = a[rkey - 1]
-                    a[rkey - 1] = a[rkey]
-                    a[rkey] = temp
+                    a[rkey - 1], a[rkey] = a[rkey], a[rkey - 1]
                 else:
                     break
-        print(a)
 
 a = [2, 8, 6, 4, 7, 4, 4, 123, 14, 5, 52, 23]
 selection_sort(a)
