7fcfa3b5031c2994a4e82d1a75b22f018bf0dfbc
ik <igor.kliushnikov@hotmail.com>
2016-06-17 16:54:31 +0300

    task14. KMP + Rabin-Karp

---
+++ tasks/ihor_kliushnikov/task14/kmp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var text = "ABC ABCDAB ABCDABCDABDE";
+            var word = "ABCDABD";
+
+            var T = new int[word.Length];
+            int pos = 2, cnd = 0;
+            T[0] = -1;
+
+            while (pos < word.Length)
+            {
+                if (word[pos - 1] == word[cnd])
+                    T[pos++] = ++cnd;
+                else if (cnd > 0)
+                    cnd = T[cnd];
+                else
+                    T[pos++] = 0;
+            }
+
+            int m = 0, i = 0;
+            while (m + i < text.Length && i < word.Length)
+            {
+                if (word[i] == text[m + i])
+                    i++;
+                else
+                {
+                    m = m + i - T[i];
+                    i = T[i] > -1
+                        ? T[i]
+                        : 0;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
